31558aaf344e78f1fa80c2484e0816e7c3a4eecc
IDKVOODOO <89485357+IDKVOODOO@users.noreply.github.com>
2026-09-08 22:27:04 +0100

    deleted file to stop right sidebar

--- javascripts/discourse/api-initializers/track-recent-topics.js
+++
@@ -1,8 +0,0 @@
-import { apiInitializer } from "discourse/lib/api";
-
-export default apiInitializer((api) => {
-  const recentTopics = api.container.lookup("service:recent-topics");
-  const appEvents = api.container.lookup("service:app-events");
-
-  appEvents.on("page:topic-loaded", recentTopics, "recordVisit");
-});
